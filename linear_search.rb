6592ca5847f41b9fb64e1ad2d48276fbeef14613
Robbie Smith <Robbie-Smith@users.noreply.github.com>
2016-10-07 12:36:02 -0600

    Updated Linear_search.rb
    Moved the method into a class.

--- linear_search.rb
+++ linear_search.rb
@@ -1,11 +1,9 @@
 require 'pry'
-def find_value(num)
-  values = (1..1000).to_a.shuffle
-  found = false
-  values.each do |value|
-    found = true if value.eql?(num)
+class LinearSearch
+  def find_value(num)
+    values = (1..1000).to_a.shuffle
+    found = false
+    values.each {|value| found = true if value.eql?(num)}
+    puts "num" if found.eql?(true)
   end
-  puts "num" if found.eql?(true)
 end
-
-find_value(25)
