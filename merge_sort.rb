afdc870f42ff7c5f12c3975057b5903f1c99c38b
Robert Smith <robert.smith160@icloud.com>
2016-10-07 09:09:07 -0600

    Update MergeSort still a bug to fix though.

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,6 +1,10 @@
 require 'pry'
 class MergeSort
 
+  def initialize
+    @final = []
+  end
+
   def sort(array)
     sort = []
     array.each_slice(2) do |value|
@@ -11,31 +15,56 @@ class MergeSort
   end
 
   def merge(sort)
-    final = [sort.delete_at([0][0])]
-    final.flatten!
-    binding.pry
-    sort.each_with_index do |value,index|
-      (final.length).times do |v|
-        index = 0 if value[index].nil?
-        final_index = 0
-        while final_index < final.length
-            if value[index] <= final[final_index]
-                final.insert(final_index,value[index])
-                value.delete_at(index)
-                break
-            elsif final_index == final.length-1
-                final.insert(final_index+1,value[index])
-                value.delete_at(index)
-                break
-            end
-            final_index += 1
-        end
-        break if sort[0].empty? && sort[1].empty?
+    final_index = 0
+    sort.map.with_index do |(value), index|
+      final_index = @final.length - 1
+      @final = [sort[index].shift] if @final.empty?
+      if (@final[final_index] <=> value).eql?(1)
+        @final.insert(final_index,value)
+        sort[index].shift
+      elsif (@final[final_index] <=> value).eql?(-1)
+        @final.insert(final_index + 1,value)
+        sort[index].shift
       end
     end
+    merge(sort) unless sort.last.empty?
   end
+
 end
-
-
 sorter = MergeSort.new
-sorter.sort(["d", "b", "a", "c","f","e"])
+sorter.sort(["d", "b", "a", "c",'e','f'])
+require 'pry'
+class MergeSort
+
+  def initialize
+    @final = []
+  end
+
+  def sort(array)
+    sort = []
+    array.each_slice(2) do |value|
+      value = value.rotate if value[0] > value[1]
+      sort << value
+    end
+    merge(sort)
+  end
+
+  def merge(sort)
+    final_index = 0
+    sort.map.with_index do |(value), index|
+      final_index = @final.length - 1
+      @final = [sort[index].shift] if @final.empty?
+      if (@final[final_index] <=> value).eql?(1)
+        @final.insert(final_index,value)
+        sort[index].shift
+      elsif (@final[final_index] <=> value).eql?(-1)
+        @final.insert(final_index + 1,value)
+        sort[index].shift
+      end
+    end
+    merge(sort) unless sort.last.empty?
+  end
+
+end
+sorter = MergeSort.new
+sorter.sort(["d", "b", "a", "c",'e','f'])
