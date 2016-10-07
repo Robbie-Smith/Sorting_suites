de9130646e2c70444623bc8a54b96e9a3452b6b5
Robert Smith <robert.smith160@icloud.com>
2016-10-07 12:11:08 -0600

    Removed some unnecessary lines and cleaned this up a bit more.

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -5,13 +5,12 @@ class InsertionSort
     sorted_set = [array.shift]
     array.cycle(array.length-1) do |element|
       sorted_set_index = 0
-      index = array.index(element)
         while sorted_set[sorted_set_index] < element
           sorted_set_index += 1
             break if sorted_set[sorted_set_index].nil?
         end
-        sorted_set.insert(sorted_set_index,element)
-        array.delete_at(index)
+        sorted_set.insert(sorted_set_index,array.delete(element))
+        break if array.empty?
     end
   end
 
