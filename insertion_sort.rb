08c4d60570330d4e87fff0d171bf8e25b09784c3
Robbie Smith <Robbie-Smith@users.noreply.github.com>
2016-10-07 12:35:19 -0600

    Removed a break, and cleaned this up a bit more.

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
 class InsertionSort
+
   def sort(array)
     sorted_set = [array.shift]
     array.cycle(array.length-1) do |element|
       sorted_set_index = 0
         while sorted_set[sorted_set_index] < element
           sorted_set_index += 1
-            break if sorted_set[sorted_set_index].nil?
+          break if sorted_set[sorted_set_index].nil?
         end
         sorted_set.insert(sorted_set_index,array.delete(element))
-        break if array.empty?
     end
+    print sorted_set
   end
 
 end
-
-sorter = InsertionSort.new
-sorter.sort(["a", "c", "z", "f", "e", "b", "d"])
+array = (1..25).to_a.shuffle
+InsertionSort.new.sort(array)
