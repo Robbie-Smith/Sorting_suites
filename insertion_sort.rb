df095c94b51d562a66256b456e9bed49db8d6ce2
Robert Smith <robert.smith160@icloud.com>
2016-10-21 15:39:32 -0600

    "Removed unnecessary lines."

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -6,16 +6,15 @@ class InsertionSort
     sorted_set = [array.shift]
     array.cycle(array.length-1) do |element|
       sorted_set_index = 0
-        while sorted_set[sorted_set_index] < element
-          sorted_set_index += 1
-          break if sorted_set[sorted_set_index].nil?
-        end
-        sorted_set.insert(sorted_set_index,array.delete(element))
+      while sorted_set[sorted_set_index] < element
+        sorted_set_index += 1
+        break if sorted_set[sorted_set_index].nil?
+      end
+      sorted_set.insert(sorted_set_index,array.delete(element))
     end
     print sorted_set
   end
-
 end
+
 array = (1..25).to_a.shuffle
 InsertionSort.new.sort(array)
-
